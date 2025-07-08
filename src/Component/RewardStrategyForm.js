d2dbd28d7aff02e5fc640ca6f88b13b63aa24b4b
megharaj-bigcity <megharaj.k@bigcity.in>
2025-07-08 19:16:54 +0530

    Update RewardStrategyForm.js
    test button is removed

--- src/Component/RewardStrategyForm.js
+++ src/Component/RewardStrategyForm.js
@@ -342,13 +342,6 @@ const RewardStrategyForm = () => {
               )}
             </button>
             
-            <button
-              type="button"
-              onClick={testWebhook}
-              className="px-4 py-2 bg-gray-200 text-gray-700 rounded-lg hover:bg-gray-300 transition-colors"
-            >
-              Test Connection
-            </button>
           </div>
         </div>
 
